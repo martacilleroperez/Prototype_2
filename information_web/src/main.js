cf16780c487bb3cfc86746bcb285645d1ac986de
mc <m.cilleroperez@student.utwente.nl>
2026-05-08 10:35:09 +0200

    Info fixed

--- information_web/src/main.js
+++ information_web/src/main.js
@@ -1,34 +1,32 @@
-
-
 document.querySelector('#app').innerHTML = `
 <section id="center">
   <img class="layer bg" src="/Prototype_2/images/intro_option_2.png" alt="Background">
 </section>
 <div class="fab-container">
-  <button class="fab-btn" data-target="./characters/Pull">
+  <button class="fab-btn" data-target="/Prototype_2/information_web/characters/Pull.html">
     <img src="/Prototype_2/images/btn_arlech.png" alt="Char 1">
   </button>
-  <button class="fab-btn" data-target="./characters/Sold">
+  <button class="fab-btn" data-target="/Prototype_2/information_web/characters/Sold.html">
     <img src="/Prototype_2/images/btn_soldier.png" alt="Char 3">
   </button>
 </div>
 `
 
-// Mask buttons + black screen
-const overlay = document.getElementById("black-overlay");
+requestAnimationFrame(() => {
+  const overlay = document.getElementById("black-overlay");
 
-if (overlay) {
   document.querySelectorAll(".fab-btn").forEach((btn) => {
     btn.addEventListener("click", () => {
-
       const target = btn.dataset.target;
-
-      overlay.classList.add("active");
-
-      setTimeout(() => {
+      console.log("navigating to:", target);
+      if (overlay) {
+        overlay.classList.add("active");
+        setTimeout(() => {
+          window.location.href = target;
+        }, 1000);
+      } else {
         window.location.href = target;
-      }, 1000);
-
+      }
     });
   });
-}
+});
