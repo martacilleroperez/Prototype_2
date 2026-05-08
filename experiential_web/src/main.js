c4acdbacf954b8df9aff8bf2c23ea5e7165b8a8a
mc <m.cilleroperez@student.utwente.nl>
2026-05-08 11:31:35 +0200

    experimental

--- experiential_web/src/main.js
+++ experiential_web/src/main.js
@@ -1,57 +1,30 @@
-import '../../public/style.css'
-
 document.querySelector('#app').innerHTML = `
 <section id="center">
-
-  <div id="black-overlay"></div>
-
-  <img 
-    class="layer bg" 
-    src="/images/intro_option_2.png" 
-    alt="Background"
-  >
-
+  <img class="layer bg" src="/Prototype_2/images/intro_option_3.png" alt="Background">
 </section>
-
-<!-- buttons container -->
 <div class="fab-container">
-
-  <button 
-    class="fab-btn" 
-    data-target="/experiential_web/characters/Pull.html"
-  >
-    <img src="/images/btn_arlech.png" alt="Char 1">
+  <button class="fab-btn" data-target="/Prototype_2/experiential_web/characters/Pull.html">
+    <img src="/Prototype_2/images/btn_arlech.png" alt="Char 1">
   </button>
- 
-  <button 
-    class="fab-btn" 
-    data-target="/experiential_web/characters/Sold.html"
-  >
-    <img src="/images/btn_soldier.png" alt="Char 3">
+  <button class="fab-btn" data-target="/Prototype_2/experiential_web/characters/Sold.html">
+    <img src="/Prototype_2/images/btn_soldier.png" alt="Char 3">
   </button>
-
 </div>
 `
 
-// Black overlay
-const overlay = document.getElementById("black-overlay");
-
-if (overlay) {
-
+requestAnimationFrame(() => {
+  const overlay = document.getElementById("black-overlay");
   document.querySelectorAll(".fab-btn").forEach((btn) => {
-
     btn.addEventListener("click", () => {
-
       const target = btn.dataset.target;
-
-      overlay.classList.add("active");
-
-      setTimeout(() => {
+      if (overlay) {
+        overlay.classList.add("active");
+        setTimeout(() => {
+          window.location.href = target;
+        }, 1000);
+      } else {
         window.location.href = target;
-      }, 1000);
-
+      }
     });
-
   });
-
-}
+});
