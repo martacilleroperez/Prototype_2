f8abe6d2453c894b0bc799f8019c34431b4e634e
mc <m.cilleroperez@student.utwente.nl>
2026-05-08 10:40:32 +0200

    Info fixed

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,18 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/Prototype_2/',
+  publicDir: 'public',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        informational: resolve(__dirname, 'information_web/index.html'),
+        experiential: resolve(__dirname, 'experiential_web/index.html'),
+        pull: resolve(__dirname, 'information_web/characters/Pull.html'),
+        sold: resolve(__dirname, 'information_web/characters/Sold.html'),
+      }
+    }
+  }
 })
